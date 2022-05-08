c3491cb41b84268c72e4c30da19ac9e43f0e78bc
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-05-08 20:00:03 -0300

    Estructura inicial

---
+++ TP5_Grupo10/src/main/java/ar/edu/unju/fi/tp5/Tp5Grupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp5Grupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp5Grupo10Application.class, args);
+	}
+
+}
---
+++ TP5_Grupo10/src/test/java/ar/edu/unju/fi/tp5/Tp5Grupo10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp5Grupo10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
