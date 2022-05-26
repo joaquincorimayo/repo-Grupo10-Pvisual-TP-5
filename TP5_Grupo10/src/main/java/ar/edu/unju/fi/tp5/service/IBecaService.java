af2e24e4d1cf54ee264b1885aed82ba5c3bae504
fv322 <38471258@fi.unju.edu.ar>
2022-05-25 22:27:03 -0300

    Desarrollo de la actividad 3 tp5-servicios.

---
+++ TP5_Grupo10/src/main/java/ar/edu/unju/fi/tp5/service/IBecaService.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.tp5.service;
+
+import ar.edu.unju.fi.tp5.model.Beca;
+import ar.edu.unju.fi.tp5.util.ListaBecas;
+
+public interface IBecaService {
+	
+	public Beca getBeca();
+	public boolean guardarBeca(Beca beca);
+	public void modificarBeca(Beca beca);
+	public void eliminarBeca(int codigo);
+	public ListaBecas getListaBecas();
+	public Beca buscarBeca(int codigo);
+
+}
